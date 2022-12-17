b925712c5458cc74773a32f7f67cd0d387f5a6b0
Rahul Kulkarni <rahulkulkarni5234@gmail.com>
2022-12-17 14:23:23 +0530

    mumbai deploye

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: "https://ultra-broken-valley.matic-testnet.discover.quiknode.pro/a05691b265c12ab2d8202b9d181707d6b00eff73/",
+      accounts: ["a640fabb998090c3f4aebcb3fc36021c86b206b8e38d324854f518bd7dc33711"],
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,34 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const main = async () => {
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  const domainContract = await domainContractFactory.deploy("rk");
+  await domainContract.deployed();
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  console.log("Contract deployed to:", domainContract.address);
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
+  let txn = await domainContract.register("monkey",  {value: hre.ethers.utils.parseEther('0.1')});
+  await txn.wait();
+  console.log("Minted domain monkey.rk");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  txn = await domainContract.setRecord("monkey", "Am I a monkey or a rk??");
+  await txn.wait();
+  console.log("Set record for monkey.rk");
 
-  await lock.deployed();
+  const address = await domainContract.getAddress("monkey");
+  console.log("Owner of domain monkey:", address);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
